fdcb411c6d27596602612d08f6659ad08f97ffa5
kartikmax <kyadav5213@gmail.com>
2024-12-05 18:25:47 +0530

    downloaded the assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Feeds from "./pages/Feeds";
+import EditProfile from "./pages/EditProfile";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/feeds" element={<Feeds />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/createPosts" element={<EditProfile />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
